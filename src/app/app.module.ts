d8d422a203a7bc160c6e6429d2860090ef6a1edf
Ana-Rodrigs <mariaanarodrigues2005@gmail.com>
2023-06-19 22:35:00 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { StudentComponent } from './student/student.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherComponent,
+    StudentComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
