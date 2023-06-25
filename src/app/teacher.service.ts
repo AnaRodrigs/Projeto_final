16a8246a6eebf75ac293583d7593cf84a9658b77
Ana-Rodrigs <mariaanarodrigues2005@gmail.com>
2023-06-25 13:48:35 -0300

    c teacher okay c student beggin'

--- src/app/teacher.service.ts
+++ src/app/teacher.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Teacher } from './teacher';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +9,22 @@ import { Injectable } from '@angular/core';
 export class TeacherService {
 
   url= "http://localhost:3000/Teacher";
-  constructor() { }
+  constructor(private http: HttpClient)  { }
+  
+  getTeachers(): Observable<Teacher[]>{
+    return this.http.get<Teacher[]>(this.url);
+   }
+   save (teacher : Teacher): Observable<Teacher>{
+    return this.http.post<Teacher>(this.url, teacher);
+   }
+   delete (teacher : Teacher): Observable<void>{
+    return this.http.delete<void>(`${this.url}/${teacher.id}`);
+   }
+   update (teacher : Teacher): Observable<Teacher>{
+    return this.http.put<Teacher>(`${this.url}/${teacher.id}`, teacher);
+   }
+   clean (teacher : Teacher): Observable<void>{
+    return this.http.delete<void>(`${this.url}/${teacher.id}`);
+   }
+
 }
--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -10,8 +10,57 @@ import { TeacherService } from '../teacher.service';
 export class TeacherComponent {
 teacher: Teacher = {} as Teacher;
 teachers : Teacher [] = [];
+isEditing : boolean = false;
 
 constructor(private teacherService: TeacherService){}
 
 
+ngOnInit(): void {
+  this.loadTeacher();
+ }
+ loadTeacher() {
+   this.teacherService.getTeachers(). subscribe(
+     {
+       next : data => this.teachers = data
+     }
+   );
+ }
+
+ onSaveEvent (teacher : Teacher){
+   if (this.isEditing)
+   {
+     this.teacherService.update(teacher).subscribe(
+       {
+         next: () => {
+           this.loadTeacher();
+           this.isEditing = false;
+         }
+       }
+     )
+   }
+   else{
+   this.teacherService.save(teacher).subscribe(
+     {
+       next : data => {
+         this.teachers.push(data);
+
+       }
+     }
+   );
+ }
+}
+edit (teacher : Teacher) {
+this.teacher = teacher;
+this.isEditing = true;
+
+}
+
+ onCleanEvent (){
+ this.isEditing = false;
+ }
+ delete (teacher : Teacher) {
+  this.teacherService.delete(teacher).subscribe({
+  next: () => this.loadTeacher()
+    })
+}
 }
